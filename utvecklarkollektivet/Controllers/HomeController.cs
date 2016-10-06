1560963fb341ae0b0624bb6ad7fbac0bab1d647c
Markus Stenqvist <daysnine@gmail.com>
2016-10-06 22:28:01 +0200

    Add how page and Google Analytics

--- utvecklarkollektivet/Controllers/HomeController.cs
+++ utvecklarkollektivet/Controllers/HomeController.cs
@@ -15,15 +15,16 @@ namespace utvecklarkollektivet.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            return View();
+        }
 
+        public ActionResult How()
+        {
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
